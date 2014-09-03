97d3dbfa69ab4195a7476f5f690d601559faf3cc
Sean Toner <placeoftheway@gmail.com>
2014-09-03 10:57:59 -0400

    Added some helper functions to parse through response for token

--- src/shi/rest.clj
+++ src/shi/rest.clj
@@ -2,7 +2,12 @@
 
 (ns shi.rest
   (:require [org.httpkit.client :as http])
-  (:require [cheshire.core]))
+  (:require [cheshire.core])
+  (:require [clojure.tools.logging :as log]))
+
+(defrecord Credentials [user password tenant token])
+
+(def cred (Credentials. "admin" "redhat" "demo" "http://192.168.122.141:5000/v2.0/tokens"))
 
 (def options {:timeout 200             ; ms
               :basic-auth ["admin" "pass"]
@@ -16,74 +21,70 @@
                     ])
 
 
-
-;; Example of what org.httpkit.client/request contains
-(let [request {:url "http://http-kit.org/"
-               :method :get             ; :post :put :head or other
-               :user-agent "User-Agent string"
-               :oauth-token "your-token"
-               :headers {"X-header" "value"
-                         "X-Api-Version" "2"}
-               :query-params {"q" "foo, bar"} ;"Nested" query parameters are also supported
-               :form-params {"q" "foo, bar"} ; just like query-params, except sent in the body
-               :basic-auth ["user" "pass"]
-               :keepalive 3000          ; Keep the TCP connection for 3000ms
-               :timeout 1000      ; connection timeout and reading timeout 1000ms
-               :filter (http/max-body-filter (* 1024 100)) ; reject if body is more than 100k
-               :insecure? true ; Need to contact a server with an untrusted SSL cert?
-
-               ;; File upload. :content can be a java.io.File, java.io.InputStream, String
-               ;; It read the whole content before send them to server:
-               ;; should be used when the file is small, say, a few megabytes
-               :multipart [{:name "comment" :content "httpkit's project.clj"}
-                           {:name "file" :content (clojure.java.io/file "project.clj") :filename "project.clj"}]
-
-               :max-redirects 10 ; Max redirects to follow
-                ;; whether follow 301/302 redirects automatically, default to true
-                ;; :trace-redirects will contain the chain of the redirections followed.
-               :follow-redirects false
-               } ])
-
 (defn create-authcreds [username pwd tenant]
-  (cheshire.core/generate-string {:auth {:tenantName tenant
-                                         :passwordCredentials {:username username
-                                                               :password pwd}}}))
+  (cheshire.core/generate-string
+   {:auth {:tenantName tenant
+           :passwordCredentials {:username username
+                                 :password pwd}}}))
 
-(defn sanitize-url [auth-url]
-  "Makes sure that the auth-url ends in /tokens"
-  (cond
-   (.endsWith auth-url "/tokens") auth-url
-   (.endsWith auth-url "/") (str auth-url "tokens")
-   :else (str auth-url "/tokens")))
+(defn sanitize-url [auth-url end]
+  "Makes sure that the auth-url ends in end arg"
+  (let [ending (format "/%s" end)]
+    (cond
+      (.endsWith auth-url ending) auth-url
+      (.endsWith auth-url "/") (str auth-url end)
+      :else (str auth-url ending))))
 
 
 (defn get-token
-  "function to retrieve token from keystone"
+  ;; uses a Credential type object
+  ([{:keys [user password tenant token]}]
+   (do
+     (log/info user password tenant token)
+     (get-token user password tenant token)))
   ([user pwd tenant auth-url]
-    (let [url
-          auth (create-authcreds user pwd tenant)
-          req {:headers {"Content-Type" "application/json", "Accept" "application/json"}
-               :body auth}]
-      @(http/post auth-url req)))
-  ([& args])
+   (let [url (sanitize-url auth-url "tokens")
+         auth (create-authcreds user pwd tenant)
+         req {:headers {"Content-Type" "application/json", "Accept" "application/json"}
+              :body auth}]
+     (log/debugf "url:%s\nauth:%s\nreq:%s" url auth (str req))
+     @(http/post url req)))
+  ;; metadata
+  {:doc "Function to retrieve token from keystone"})
 
 
-(defn endpoint-convert [m]
+(quote "TODO: the serviceCatalog isn't structured conveniently, so convert it to a
+       map keyed by the service name, rather than as an array of services"
+(defn endpoint-convert [ctlg]
   "Return a transformed map"
   ; FIXME: I think we can make this a macro
-  (let [newname (str (m "name") "-" (get-in catalog [0 "endpoints" 0 "id"]))]
-    {newname (dissoc m "name")}))
+  (loop [[ep & r] ctlg]
+    (if ep
+      ))
+)
 
 
-
-(defn parse-token-resp [resp]
-  "Take the :body from our response and break it up into the parts we need"
-  (let [body (cheshire.core/parse-string (:body resp))]
-    ; Get the endpoints
-    ))
-
-
+(defn get-from-body [body & args]
+  "retrieve an element from a nested data structure
+  usage:
+  (def m {:order {:id 1001 :date 12012014}})
+  (get-from-body m :order :id)"
+  (get-in body args))
 
 
+(defn get-body [resp]
+  "Take the :body from our response and convert to a map"
+  (cheshire.core/parse-string (:body resp)))
+
+
+(defn parse-resp [resp]
+  "Returns a map where :body is a map instead of a string"
+  (assoc resp :body (get-body resp)))
+
+
+(defn get-catalog [resp]
+  "Gets the service catalog from within the body"
+  (let [r (parse-resp resp)]
+    (-> (:body r) (get-from-body "access" "serviceCatalog"))))
 
 
